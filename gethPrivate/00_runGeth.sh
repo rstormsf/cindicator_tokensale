af40f3223b6a3a75dbb75941ae282f06981aa568
Roman Storm <rstormsf@gmail.com>
2017-09-06 18:26:32 -0700

    add reports

--- gethPrivate/00_runGeth.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -rf ./gethPrivate/testchain/geth/chaindata/*
-
-geth --datadir ./gethPrivate/testchain init ./gethPrivate/genesis.json
-sleep 5
-geth --datadir ./gethPrivate/testchain --unlock 0 --password ./gethPrivate/testpassword --rpc --rpccorsdomain '*' --rpcport 8646 --rpcapi "eth,net,web3,debug,personal" --port 32323 --mine --minerthreads 8 --maxpeers 0 --targetgaslimit 994712388 2>> /dev/null >> logs.txt
--- gethPrivate/01_runTruffleGeth.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sh gethPrivate/00_runGeth.sh & 
-sleep 10
-./node_modules/.bin/truffle test --network geth
-killall geth
